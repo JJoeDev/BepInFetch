b992d8705c841d79d5b21ae1ff0ce337556eadc0
JJoe <48448023+jona939s@users.noreply.github.com>
2025-07-29 01:15:48 +0200

    Robust build script

--- build.bat
+++ build.bat
@@ -2,7 +2,24 @@
 
 cls
 
-cmake -G Ninja .
+REM Default build type is debug
+set BUILD_TYPE=-DCMAKE_BUILD_TYPE=Debug
+
+REM Override the default build type if arg is given
+if not "%1"=="" (
+	if /i "%1"=="release" set BUILD_TYPE=-DCMAKE_BUILD_TYPE=Release
+)
+
+cmake -G Ninja %BUILD_TYPE% .
+if errorlevel 1 (
+	echo CMake configuration failed!
+	exit /b 1
+)
+
 ninja
+if errorlevel 1 (
+	echo Ninja build failed!
+	exit /b 1
+)
 
 .\bin\BepFetch.exe
